7d5ed992225ed7b936cb851ca6000d5f46d930c1
Chibuikem Madugba <chibuikemmadugba@gmail.com>
2026-07-30 04:03:40 +0000

    ci: add code coverage reporting and artifact upload (#97)

--- horizon-listener/jest.config.js
+++ horizon-listener/jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageReporters: ['text', 'lcov', 'json-summary'],
 };
